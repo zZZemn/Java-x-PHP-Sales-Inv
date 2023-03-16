59f31511671c38b839601347b1c3bde361028b94
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-03-16 23:25:18 +0800

    orders

--- orders.php
+++ orders.php
@@ -1,37 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Orders</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <ul class="nav nav-tabs mt-5 w-75 nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link nav-bold" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="item.php">Item</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="sales.php">Sales</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="orders.php">Orders</a>
-        </li>
-    </ul>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Orders</title>
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+            crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <ul class="nav nav-tabs mt-5 w-75 nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link nav-bold" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="item.php">Item</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="sales.php">Sales</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="orders.php">Orders</a>
+            </li>
+        </ul>
 
-    <div class="container">
-        <form class="container">
-            
-        </form>
+        <div class="container">
+            <form class="mt-4 bg-primary p-3">
+                <div class="d-flex justify-content-around">
+                    <div class="">
+                        <input type="text" class="form-control" placeholder="Customer" name="customer">
+                    </div>
 
+                    <div class="">
+                        <input type="text" class="form-control" placeholder="Address" name="address">
+                    </div>
+                    <div class="">
+                        <input type="text" class="form-control" placeholder="Contact no" name="contact">
+                    </div>
 
-    </div>
+                    <div class="">
+                        <select class="form-control">
+                            <option>COD</option>
+                            <option>Gcash</option>
+                            <option>Maya</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-</body>
+        </div>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+            crossorigin="anonymous"></script>
+    </body>
 </html>
